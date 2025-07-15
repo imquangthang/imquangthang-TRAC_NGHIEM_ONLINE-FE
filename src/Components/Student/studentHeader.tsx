d09dd0afdb797b42ecec9240907122c7c91d0c18
ACER <ACER@IMTHANG>
2025-07-15 15:31:16 +0700

    feat: add user update and delete modals, enhance user management functionality

--- src/Components/Student/studentHeader.tsx
+++ src/Components/Student/studentHeader.tsx
@@ -121,7 +121,7 @@ const StudentHeader = () => {
                 <Link
                   to="/login"
                   onClick={() => {
-                    localStorage.removeItem("token");
+                    localStorage.removeItem("jwt");
                     localStorage.removeItem("user");
                   }}
                   className="block px-4 py-2 font-medium text-sm text-gray-700 hover:text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
