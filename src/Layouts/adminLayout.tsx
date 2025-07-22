6df1e78628e17c7f11af6687794de67d6e47d389
ACER <ACER@IMTHANG>
2025-07-22 14:50:28 +0700

    chore: remove redundant UI

--- src/Layouts/adminLayout.tsx
+++ src/Layouts/adminLayout.tsx
@@ -1,21 +1,3 @@
-// import { Outlet } from "react-router-dom";
-// import AdminNav from "../Components/Navbar/adminNav";
-
-// const adminLayout = () => (
-//   <>
-//     <div className="flex bg-white dark:bg-black">
-//       <div style={{ width: "22%" }}>
-//         <AdminNav />
-//       </div>
-//       <div className="mx-2 relative" style={{ width: "77%" }}>
-//         <Outlet />
-//       </div>
-//     </div>
-//   </>
-// );
-
-// export default adminLayout;
-
 import { Outlet } from "react-router-dom";
 import AdminNav from "../Components/Navbar/adminNav";
 import { useState } from "react";
--- src/Layouts/teacherLayout.tsx
+++ src/Layouts/teacherLayout.tsx
@@ -1,19 +1,3 @@
-// import { Outlet } from "react-router-dom";
-
-// const teacherLayout = () => (
-//   <>
-//     <div className="flex bg-white dark:bg-black  ">
-//       <div style={{ width: "22%" }}>
-//         <TeacherNav />
-//       </div>
-//       <div className="mx-2 relative" style={{ width: "77%" }}>
-//         <Outlet />
-//       </div>
-//     </div>
-//   </>
-// );
-
-// export default teacherLayout;
 import { Outlet } from "react-router-dom";
 import TeacherNav from "../Components/Navbar/teacherNav";
 import { useState } from "react";
