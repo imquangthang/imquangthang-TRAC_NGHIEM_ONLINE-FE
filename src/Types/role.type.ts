5138f3fd2283634776a301d340aae7859323e4a8
Lê Văn Đức Huy <lvdhuy0412@gmail.com>
2025-09-18 08:36:19 +0700

    role

---
+++ src/Types/role.type.ts
@@ -0,0 +1,11 @@
+export type Role ={
+  id: number;
+  title: string;
+  description: string;
+};
+
+export type RoleData ={
+  id?: number;            
+  title: string;
+  description: string;
+};
